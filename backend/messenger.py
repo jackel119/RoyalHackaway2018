1961fa8cf461b847ff4b8f13cd472c98ec86a3a6
Jack Pordi <pp2916@ic.ac.uk>
2018-01-13 17:51:36 +0000

    Messenger interface now mostly complete and ready

--- backend/messenger.py
+++ backend/messenger.py
@@ -1,4 +1,6 @@
+from datetime import datetime
 import time
+import json
 from fbchat import Client
 from fbchat.models import *
 
@@ -14,16 +16,38 @@ def get_messages():
     user = client.searchForGroups("Socialist")[0]
     return client.fetchThreadMessages(thread_id=user.uid, limit=1000)
 
-username, login = get_login('login')
-client = Client(username, login)
-print("Input the user you want to message:")
-to_search = input()
-users = client.searchForUsers(to_search) + client.searchForGroups(to_search)
-users = users
-for i in range(0,len(users)):
-    print(i, ":", users[i].name)
-user = users[int(input("Please specify which chat you'd like to participate in:"))]
-for message in client.fetchThreadMessages(thread_id=user.uid, limit=1000)[::-1]:
-    print(message.author, message.text)
+class Messenger(object):
 
-# 100002663745605
+    def __init__(self):
+        username, login = get_login('login')
+        self.client = Client(username, login)
+        self.user_map = {}
+        self._initialize_contacts()
+
+    def _initialize_contacts(self):
+        self.user_map[self.client.uid] = self.client.fetchUserInfo(self.client.uid)[self.client.uid].name
+        for user in self.client.fetchAllUsers():
+            self.user_map[user.uid] = user.name
+
+    def run_loop(self):
+        print("Input the user you want to message:")
+        to_search = input()
+        users = self.client.searchForUsers(to_search) + self.client.searchForGroups(to_search)
+        users = users
+        for i in range(0,len(users)):
+            print(i, ":", users[i].name)
+        user = users[int(input("Please specify which chat you'd like to participate in:"))]
+        for message in self.client.fetchThreadMessages(thread_id=user.uid, limit=1000)[::-1]:
+            if message.text:
+                print(datetime.fromtimestamp(int(message.timestamp) // 1000).strftime('%Y-%m-%d %H:%M:%S'), self.user_map[message.author], message.text)
+
+    def test(self):
+        print(self.client.fetchAllUsers())
+        print(type(self.client.fetchAllUsers()[0]))
+
+
+
+if __name__ == '__main__':
+    m = Messenger()
+    #print(json.dumps(m.user_map, indent=4, sort_keys=True))
+    m.run_loop()
