1f9eb222deb110eb28baa06c9ecf292c372e8137
elt16 <elt16@ic.ac.uk>
2018-01-13 23:19:35 +0000

    Added addressee functionality, needs debugging

--- backend/chat.py
+++ backend/chat.py
@@ -18,14 +18,14 @@ class Chat(object):
             self.isGroup = True
         self.participants[owner_id] =  owner_name
         self.queries = []
-        #self.generate()
+        self.generate()
 
     def generate(self):
         for message in self.messages:
             if message.text:
                 message.sanitized = sanitize(message.text)
                 # Generate Query
-                q = isQuery(message)
+                q = self.isQuery(message)
                 if q:
                     self.queries.append(q)
                 else:
@@ -34,6 +34,48 @@ class Chat(object):
 
     def show_queries(self):
         print(self.queries)
+
+    def isQuery(self, message):
+        query = Query()
+        tagged = nltk.pos_tag(nltk.word_tokenize(message.sanitized))
+        firstTag = tagged[0][1]
+        if firstTag == "CC" or firstTag == "RB":
+            tagged = tagged[1:]
+            if not tagged:
+                return
+        wqWord = isWQuestion(tagged[0][0])
+        if wqWord:
+            query.qtype = wqWord
+        else:
+            for word, tag in tagged:
+                if word == "?":
+                    query.qtype = QType.BOOL
+                    break
+        if query.qtype:
+            #Checking the addressee of the question
+            if message.mentions:
+                query.addressee = message.mentions[0]
+                #see if there are group users' names in the message i.e. jack, dima, natalia ... for The New Socialist State
+                #if there are pronouns AND names in the message, check if the name is a user in the chat
+                #if the name is NOT a user, then the addressee will be the PRONOUN
+                #pronouns: replace I/me/etc with author, replace you with last person, replace he/she/them with relevant people if possible (maybe the last name mentioned in the chat?)
+            else:
+                names = list(filter(lambda x : x[0] in self.participants.values, tagged))
+                if names:
+                    query.addressee = names[0]
+
+            #Checking the clause of the question
+            query.clause = list(filter(lambda x : notIrrelevant(x[1]), tagged))
+            print(datetime.fromtimestamp(int(message.timestamp) // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+            print(message.text)
+            print(message.sanitized)
+            print(query.qtype)
+            print("Clause:", query.clause)
+            print("Addressee: ", query.addressee)
+            print()
+            return query
+        return 
+   
 
 replacements = {
   'y': 'why',
--- backend/query.py
+++ backend/query.py
@@ -48,41 +48,40 @@ def notIrrelevant(tag):
         return True
     return False
 
-def isQuery(message):
-    query = Query()
-    tagged = nltk.pos_tag(nltk.word_tokenize(message.sanitized))
-    firstTag = tagged[0][1]
-    if firstTag == "CC" or firstTag == "RB":
-        tagged = tagged[1:]
-        if not tagged:
-            return
-    wqWord = isWQuestion(tagged[0][0])
-    if wqWord:
-        query.qtype = wqWord
-    else:
-        for word, tag in tagged:
-            if word == "?":
-                query.qtype = QType.BOOL
-                break
-    if query.qtype:
-        #Checking the addressee of the question
-        if message.mentions:
-            query.addressee = message.mentions[0]
-        #else if:
-            #see if there are group users' names in the message i.e. jack, dima, natalia ... for The New Socialist State
-            #if there are pronouns AND names in the message, check if the name is a user in the chat
-            #if the name is NOT a user, then the addressee will be the PRONOUN
-        #else:
-            #pronouns: replace I/me/etc with author, replace you with last person, replace he/she/them with relevant people if possible (maybe the last name mentioned in the chat?)
-
-        #Checking the clause of the question
-        query.clause = list(filter(lambda x : notIrrelevant(x[1]), tagged))
-        print(datetime.fromtimestamp(int(message.timestamp) // 1000).strftime('%Y-%m-%d %H:%M:%S'))
-        print(message.text)
-        print(message.sanitized)
-        print(query.qtype)
-        print("Clause:", query.clause)
-        print()
-        return query
-    return 
+#def isQuery(message):
+#    query = Query()
+#    tagged = nltk.pos_tag(nltk.word_tokenize(message.sanitized))
+#    firstTag = tagged[0][1]
+#    if firstTag == "CC" or firstTag == "RB":
+#        tagged = tagged[1:]
+#        if not tagged:
+#            return
+#    wqWord = isWQuestion(tagged[0][0])
+#    if wqWord:
+#        query.qtype = wqWord
+#    else:
+#        for word, tag in tagged:
+#            if word == "?":
+#                query.qtype = QType.BOOL
+#                break
+#    if query.qtype:
+#        #Checking the addressee of the question
+#        if message.mentions:
+#            query.addressee = message.mentions[0]
+#            #see if there are group users' names in the message i.e. jack, dima, natalia ... for The New Socialist State
+#            #if there are pronouns AND names in the message, check if the name is a user in the chat
+#            #if the name is NOT a user, then the addressee will be the PRONOUN
+#            #pronouns: replace I/me/etc with author, replace you with last person, replace he/she/them with relevant people if possible (maybe the last name mentioned in the chat?)
+#        else:
+#            pass
+#        #Checking the clause of the question
+#        query.clause = list(filter(lambda x : notIrrelevant(x[1]), tagged))
+#        print(datetime.fromtimestamp(int(message.timestamp) // 1000).strftime('%Y-%m-%d %H:%M:%S'))
+#        print(message.text)
+#        print(message.sanitized)
+#        print(query.qtype)
+#        print("Clause:", query.clause)
+#        print()
+#        return query
+#    return 
         
