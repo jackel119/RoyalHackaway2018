151f829e985174ed4906a5eca53e9695f947657a
Jack Pordi <pp2916@ic.ac.uk>
2018-01-13 16:38:21 +0000

    Start replacing I/you clauses with author/mentions

--- backend/test.py
+++ backend/test.py
@@ -7,16 +7,9 @@ def get_login(f):
         lines =file.readlines()
     return lines[0].replace('\n', ''), lines[1].replace('\n', '')
 
-username, login = get_login('login')
-client = Client(username, login)
-users = client.fetchAllUsers()
-user = client.searchForUsers("Emma Tye")[0]
-print(user.uid)
-# while True:
-#     client.send(Message(text="Heeeyyyyyy Emmma 8====D"), int(user.uid))
-#     time.sleep(10) 
-#for message in client.fetchThreadMessages(thread_id=user.uid, limit=1000):
-#    print(message.timestamp, message.text)
-
-all_threads = client.fetchThreadList()
-print(all_threads)
+def get_messages():
+    username, login = get_login('login')
+    client = Client(username, login)
+    users = client.fetchAllUsers()
+    user = client.searchForGroups("Socialist")[0]
+    return client.fetchThreadMessages(thread_id=user.uid, limit=1000)
