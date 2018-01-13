1b03c1bfc906f3c785cab9520f1741a4af568ebb
CX-16 <dimarammfire@CX-2.local>
2018-01-13 16:37:26 +0000

    [dimasquest] Added options for testing.

--- backend/test.py
+++ backend/test.py
@@ -4,19 +4,19 @@ from fbchat.models import *
 
 def get_login(f):
     with open(f, 'r') as file:
-        lines =file.readlines()
+        lines = file.readlines()
     return lines[0].replace('\n', ''), lines[1].replace('\n', '')
 
 username, login = get_login('login')
 client = Client(username, login)
 users = client.fetchAllUsers()
-user = client.searchForUsers("Emma Tye")[0]
-print(user.uid)
-# while True:
-#     client.send(Message(text="Heeeyyyyyy Emmma 8====D"), int(user.uid))
-#     time.sleep(10) 
+print("Input the user you want to message:")
+user = client.searchForUsers(input())[0]
+print(user.uid + " is id of " + user.name)
+while True:
+    client.send(Message(text="Heeeyyyyyy" + user.name + "8====D"), int(user.uid))
+    time.sleep(10)
+    print("Sent")
 #for message in client.fetchThreadMessages(thread_id=user.uid, limit=1000):
 #    print(message.timestamp, message.text)
 
-all_threads = client.fetchThreadList()
-print(all_threads)
